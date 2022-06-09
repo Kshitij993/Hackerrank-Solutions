ccb0b1e07dcdb0dcbe8b62cc16d650e822f778f9
Kshitij Kumar Sharma <kshitijsharma993@gmail.com>
2022-06-09 20:44:45 +0530

    test

--- HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/javaaid/hackerrank/solutions/algorithms/bitmanipulation/BitManipulationLonelyInteger.java
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/javaaid/hackerrank/solutions/algorithms/bitmanipulation/BitManipulationLonelyInteger.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
  * @author Kanahaiya Gupta
  *
  */
+//to test
 public class BitManipulationLonelyInteger {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -24,4 +25,5 @@ public class BitManipulationLonelyInteger {
 		System.out.println(res);
 		sc.close();
 	}
+	
 }
